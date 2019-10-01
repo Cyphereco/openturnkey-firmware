0b06b46a873ce2ca4425cfcbaf77e6765e500a68
yorotsuya <yorotsuya@protonmail.com>
2019-10-02 00:35:29 +0900

    * Simplify low power LED alert indications. * Add 'make flash' and 'make reset' commands to support image flash and device reset.

--- main.c
+++ main.c
@@ -113,9 +113,11 @@ int main(void)
     if (_pwrLvl < 3550) {
         LED_blink(_led, 3);
     }
+#if 0   /* remove blink twice indication to simplify the LED alert states. %/ 
     else if (_pwrLvl < 3650) {
         LED_blink(_led, 2);
     }
+#endif    
     else {
         LED_on(_led);
         nrf_delay_ms(500);
